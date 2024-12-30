54e914398f69101212d9f994269c9d10d05a4306
yvanroan <yvanroan@yahoo.fr>
2024-12-29 21:03:04 -0500

    added standard namespace to string

--- src/data/array.hpp
+++ src/data/array.hpp
@@ -4,21 +4,15 @@
 template <typename T>
 template <typename Func>
 
-//Array class should represent a single Array of data
-//and should support the following:
-//      Efficient storage of homogenous types.
-//      Operations like filtering & aggregations
-//      Type safety and validation
-
 class Array{
 
     private: 
         std::vector<std::optional<T>> sequence;
         size_t size=0;
-        string name;
+        std::string name;
 
     public:
-        Array(string name);
+        Array(std::string name);
         void append(std::optional<T> data);
         void append(const Array<std::optional<T>>& col);
         void appendNull();
