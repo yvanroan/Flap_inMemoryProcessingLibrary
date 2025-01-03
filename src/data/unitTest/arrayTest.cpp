ddfbf110b6b72d1d3ebc9033e4d8d00cba463175
yvanroan <yvanroan@yahoo.fr>
2025-01-03 15:58:07 -0500

    added unittest for data section of the project but will implement it along with integration test given that the implementation of core classes (Array and Table) might change

---
+++ src/data/unitTest/arrayTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <optional>
+#include "array.hpp"
+
+// TODO: 1. use all methods in the class
+// TODO: 2. print using serialization 
+
+int main () {
+
+}
---
+++ src/data/unitTest/tableTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "array.hpp"
+#include <optional>
+#include <unordered_map>
+#include "table.hpp"
+
+// TODO: 1. use all methods in the class except joins, focus on single table interactions
+// TODO: 2. print using serialization 
+
+int main () {
+
+}
