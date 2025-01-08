0bae57a5e9768b446a8a2f2d2e7dde3e75f9481a
yvanroan <yvanroan@yahoo.fr>
2025-01-07 23:32:44 -0500

    Added validatePlan to check parameter types and ensure memory-managed objects are in memory. Integrated DiskSpiller to load spilled objects during validation. Implemented executePlan to dynamically execute QueryNode operations based on operatorType.

--- src/execution/QueryPlanner.cpp
+++ src/execution/QueryPlanner.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "QueryPlanner.hpp"
 
 
@@ -17,3 +18,97 @@ QueryNode addNode(const std::variant<Array*, Table*>& subject,
     };
 }
 
+std::vector<ParamsType> validateInputsAndGetObjects(const std::string& operatorType, const std::vector<std::any>& params) {
+    const auto& metadata = operatorMetadata.at(operatorType);
+
+    if (params.size() != metadata.size()) {
+        throw std::runtime_error("Incorrect number of parameters for operator: " + operatorType);
+    }
+
+    std::vector<ParamsType> objects;
+
+    for (size_t i = 0; i < params.size(); ++i) {
+        const auto& [paramName, paramType] = metadata[i];
+        
+        if (params[i].type() != paramType) {
+            throw std::runtime_error("Invalid type for parameter '" + paramName +
+                                     "'. Expected " + paramType.name() +
+                                     " but got " + params[i].type().name());
+        }
+
+        if (paramType == typeid(Array*) || paramType == typeid(Table*)) {
+            objects.emplace_back(params[i]);
+        }
+    }
+
+    return objects;
+}
+
+void validateQuery() {
+    for (const auto& node : queryPlan) {
+        std::visit([](auto* obj) {
+            if (!obj) throw std::runtime_error("Subject is null.");
+            if (!MemoryManager::getInstance().isInMemory(obj->getId())) {
+                DiskSpiller::getInstance().load(obj->getId()); // Load if spilled
+            }
+        }, node.subject);
+        
+        for (const auto& param : validateInputsAndGetObjects(node.operatorType, node.params)) {
+            std::visit([](auto* obj) {
+                if (obj && !MemoryManager::getInstance().isInMemory(obj->getId())) {
+                    DiskSpiller::getInstance().load(obj->getId()); // Load if spilled
+                }
+            }, param);
+        }
+    }
+}
+
+void executePlan() {
+    for (const auto& node : queryPlan) {
+        std::visit([&node](auto* obj) {
+
+            const auto& params = node.params;
+
+            if (node.operatorType == "appendCol") {
+                obj->appendCol(std::get<std::string>(params[0]), std::get<Array*>(params[1]));
+            } else if (node.operatorType == "append") {
+                obj->append(std::get<std::optional<ArrayType>>(params[0]));
+            } else if (node.operatorType == "appendNull") {
+                obj->appendNull();
+            } else if (node.operatorType == "extend") {
+                obj->extend(std::get<Array*>(params[0]));
+            } else if (node.operatorType == "map") {
+                obj->map(std::get<std::function<std::optional<ArrayType>(std::optional<ArrayType>)>>(params[0]));
+            } else if (node.operatorType == "filter") {
+                obj->filter(std::get<std::function<bool(std::optional<ArrayType>)>>(params[0]));
+            } else if (node.operatorType == "fillNulls") {
+                obj->fillNulls(std::get<ArrayType>(params[0]));
+            } else if (node.operatorType == "isNullAt") {
+                obj->isNullAt(std::get<int>(params[0]));
+            } else if (node.operatorType == "filteredIndex") {
+                obj->filteredIndex(std::get<std::function<bool(std::optional<ArrayType>)>>(params[0]));
+            } else if (node.operatorType == "aggregate") {
+                obj->aggregate(std::get<std::function<ArrayType(ArrayType, ArrayType)>>(params[0]),
+                               std::get<ArrayType>(params[1]));
+            } else if (node.operatorType == "rename") {
+                obj->rename(std::get<std::string>(params[0]), std::get<std::string>(params[1]));
+            } else if (node.operatorType == "dataAt") {
+                obj->dataAt(std::get<std::string>(params[0]), std::get<int>(params[1]));
+            } else if (node.operatorType == "innerJoin") {
+                obj->innerJoin(std::get<Table*>(params[0]), std::get<Table*>(params[1]),
+                               std::get<std::vector<std::string>>(params[2]));
+            } else if (node.operatorType == "leftJoin") {
+                obj->leftJoin(std::get<Table*>(params[0]), std::get<Table*>(params[1]),
+                              std::get<std::vector<std::string>>(params[2]));
+            } else if (node.operatorType == "rightJoin") {
+                obj->rightJoin(std::get<Table*>(params[0]), std::get<Table*>(params[1]),
+                               std::get<std::vector<std::string>>(params[2]));
+            } else if (node.operatorType == "outerJoin") {
+                obj->outerJoin(std::get<Table*>(params[0]), std::get<Table*>(params[1]),
+                               std::get<std::vector<std::string>>(params[2]));
+            } else {
+                throw std::runtime_error("Unsupported operator: " + node.operatorType);
+            }
+        }, node.subject);
+    }
+}
--- src/execution/QueryPlanner.hpp
+++ src/execution/QueryPlanner.hpp
@@ -5,10 +5,13 @@
 #include <vector>
 #include <string>
 #include <variant>
+#include <optional>
+#include <functional>
 
 class Array;
 class Table;
 
+using ArrayType = std::variant<int, float, double, std::string>;
 using ParamsType = std::variant<int, std::string, float, double, Table*, Array* >;
 
 struct QueryNode {
@@ -23,6 +26,134 @@ class QueryPlanner{
         std::vector<QueryNode> queryPlan;
         static QueryPlanner instance;
 
+        static const std::unordered_map<std::string, std::vector<std::pair<std::string, std::type_index>>> operatorMetadata = {
+            //array
+            {"append", {
+                {"additional_data", typeid(std::optional<ArrayType>)}
+            }},
+
+            {"extend", {
+                {"extraArray", typeid(Array*)}
+            }},
+
+            {"appendNull", {} },
+
+            {"map", {
+                {"mapFunc", typeid(std::function<std::optional<ArrayType>(std::optional<ArrayType>)>)}
+            }},
+            
+            {"filter", {
+                {"filterFunc", typeid(std::function<bool(std::optional<ArrayType>)>)}
+            }},
+
+            {"size", {} },
+
+            {"fillNulls", {
+                {"val", typeid(ArrayType)}
+            } },
+
+            {"isNullAt", {
+                {"idx", typeid(int)}
+            }},
+
+            {"filteredIndex", {
+                {"filterFunc", typeid(std::function<bool(std::optional<ArrayType>)>)}
+            }},
+
+            {"aggregate", {
+                {"aggFunc", typeid(std::function<ArrayType(ArrayType, ArrayType)>)},
+                {"initialValue", typeid(ArrayType)}
+            }},
+
+            //table
+            {"appendCol", {
+                {"columnName", typeid(std::string)},
+                {"inputArray", typeid(Array*)}
+            }},
+
+            {"appendRow", {
+                {"newEntry", typeid(std::vector<>)},
+            }},
+
+            {"RENAME", {
+                {"oldName", typeid(std::string)},
+                {"newName", typeid(std::string)}
+            }},
+
+            {"dataAt", {
+                {"columnName", typeid(std::string)},
+                {"rowNumber", typeid(int)},
+            }},
+
+            {"column", {
+                {"columnName", typeid(std::string)}
+            }},
+
+            {"row", {
+                {"rowNumber", typeid(int)},
+            }},
+
+            {"rows", {
+                {"indexes", typeid(std::vector<std::int>)}
+            }},
+
+            {"rows", {
+                {"startRowNumber", typeid(int)},
+                {"endRowNumber", typeid(int)},
+            }},
+
+            {"filterRow", {
+                {"filterRowFunc", typeid(std::function<bool(ArrayType)>)}
+            }},
+
+            {"filterCol", {
+                {"columnNames", typeid(std::vector<std::string>)},
+                {"remove", typeid(bool)},
+            }},
+
+            {"map", {
+                {"mapFunc", typeid(std::function<ArrayType(ArrayType)>)},
+                {"columnName", typeid(std::string)},
+            }},
+
+            {"aggregateColumn", {
+                {"columnName", typeid(std::string)},
+                {"aggFunc", typeid(std::function<ArrayType(ArrayType, ArrayType)>)},
+                {"initialValue", typeid(ArrayType)}
+            }},
+
+            {"aggregateColumns", {
+                {"columnNames", typeid(std::vector<std::string>)},
+                {"aggFunc", typeid(std::function<ArrayType(ArrayType, ArrayType)>)},
+                {"initialValue", typeid(ArrayType)}
+            }},
+
+            {"innerJoin", {
+                {"left", typeid(Table*)},
+                {"right", typeid(Table*)},
+                {"keyColumns", typeid(std::vector<std::string>)}
+            }},
+
+            {"outerJoin", {
+                {"left", typeid(Table*)},
+                {"right", typeid(Table*)},
+                {"keyColumns", typeid(std::vector<std::string>)}
+            }},
+
+            {"leftJoin", {
+                {"left", typeid(Table*)},
+                {"right", typeid(Table*)},
+                {"keyColumns", typeid(std::vector<std::string>)}
+            }},
+
+            {"rightJoin", {
+                {"left", typeid(Table*)},
+                {"right", typeid(Table*)},
+                {"keyColumns", typeid(std::vector<std::string>)}
+            }},
+
+        };
+
         QueryPlanner() = default;
 
     public: 
@@ -32,7 +163,10 @@ class QueryPlanner{
              const std::string& operatorType,
              const std::vector<ParamsType>& columns = {});
 
-        
+        std::vector<ParamsType> validateInputsAndGetObjects(const std::string& operatorType, const std::vector<std::any>& params); 
+
+        void validatePlan();
+        void executePlan();
 
 }
 
